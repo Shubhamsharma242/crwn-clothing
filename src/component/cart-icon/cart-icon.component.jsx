2542d7b10ffd754e704489946cc1d77052c1db7d
Shubhamsharma242 <shubhamsharma242242@gmail.com>
2023-06-12 18:10:37 +0530

    added the addCart feature

--- src/component/cart-icon/cart-icon.component.jsx
+++ src/component/cart-icon/cart-icon.component.jsx
@@ -3,14 +3,14 @@ import { CartContext } from "../../contexts/cart.contetxt";
 import "./cart-icon.style.scss";
 import { ReactComponent as ShopingIcon } from "../../assets/shopping-bag.svg";
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen,cartCount } = useContext(CartContext);
   const toggleIsCartOPen = () => {
     setIsCartOpen(!isCartOpen);
   };
   return (
     <div className="cart-icon-container" onClick={toggleIsCartOPen}>
       <ShopingIcon className="shoping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{cartCount}</span>
     </div>
   );
 };
--- src/component/product-card/product-card.component.jsx
+++ src/component/product-card/product-card.component.jsx
@@ -1,18 +1,21 @@
-import React from "react";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.contetxt";
 import Button from "../button/button.component";
-import './product-card.style.scss'
-const ProductCard = ({product}) => {
-
-    const {price,name,imageUrl} = product;
+import "./product-card.style.scss";
+const ProductCard = ({ product }) => {
+  const { price, name, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+  const addProductToCart = () => addItemToCart(product);
   return (
-    
     <div className="product-card-container">
-        <img src={`${imageUrl}`} alt={`${name}`} />
+      <img src={`${imageUrl}`} alt={`${name}`} />
       <div className="footer">
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-     <Button buttonType='inverted'>Add to card</Button>
+      <Button buttonType="inverted" onClick={addProductToCart}>
+        Add to card
+      </Button>
     </div>
   );
 };
